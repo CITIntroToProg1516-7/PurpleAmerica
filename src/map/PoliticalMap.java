e29e45b87f7e32d06172c4bdabb2aefa3ba7469b
hcps-vanderzrs <hcps-vanderzrs@henricostudents.org>
2016-04-21 15:34:25 -0400

    Revert "FolderFile Data Parsing/Organization "

--- src/map/PoliticalMap.java
+++ src/map/PoliticalMap.java
@@ -1,7 +1,7 @@
 /*
  * Political Map Project
- * Name: Robbie VanDerzee
- * Block: 7
+ * Name: 
+ * Block:
  * 
  * Program Purpose:
  *
@@ -11,32 +11,14 @@
  *
  */
 package map;
-
-import java.io.File;
-
+import edu.princeton.cs.introcs.*;
 /**
  *
- * @Robbie
+ * @author 
  */
 public class PoliticalMap {
-
-    public static void main(String[] args) {
-        FileList list = new FileList();
-
-        System.out.println("Data Directory must be in Users/Username/Documents/NetBeansProjects/PurpleAmerica/src/data");
-
-        final File folder = new File("C://Users/" + System.getProperty("user.name") + "/Documents/NetBeansProjects/PurpleAmerica/src/data/");
-
-        //Add library selection from dir 
-        String[] fileNames = new String[1000];
-        fileNames = list.fileList(folder);
-
-        //Parses and organizes text
-        Selection select = new Selection();
-        select.initialiseModule(fileNames);
-
+    public static void main(String[] args){
+        
+        
     }
-    //
-    //
-    //
 }
