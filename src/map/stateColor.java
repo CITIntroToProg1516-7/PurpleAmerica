a97fffa5863b2d0248e0a5ddc4e8f8ff2bc18a60
hcps-rozarioed <hcps-rozarioed@180-17ROZARIOED.hcps.internal>
2016-04-21 15:25:48 -0400

    added a function for calculating the color

--- src/map/stateColor.java
+++ src/map/stateColor.java
@@ -4,19 +4,21 @@
  * and open the template in the editor.
  */
 package map;
+
 import java.awt.Color;
+
 /**
  *
  * @author hcps-rozarioed
  */
 public class stateColor {
-    
-    public Color colorPalette(int rVotes, int dVotes, int oVotes){
-        int total = rVotes + dVotes + oVotes; 
-        int rRatio = rVotes/total;
-        int dRatio = dVotes/total; 
-        int oRatio = oVotes/total;
+
+    public Color colorPalette(int rVotes, int dVotes, int oVotes) {
+        int total = rVotes + dVotes + oVotes;
+        int rRatio = rVotes / total;
+        int dRatio = dVotes / total;
+        int oRatio = oVotes / total;
         return new Color(rRatio, dRatio, oRatio);
     }
-    
+
 }
