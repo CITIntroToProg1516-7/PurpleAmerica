6d4863ab9c5fe9b54626b7ba126f3f545c171815
hcps-vanderzrs <hcps-vanderzrs@henricostudents.org>
2016-04-28 13:57:53 -0400

    Revert "4/21 Push"

--- src/map/PoliticalMap.java
+++ src/map/PoliticalMap.java
@@ -12,15 +12,13 @@
  */
 package map;
 import edu.princeton.cs.introcs.*;
-import java.io.FileNotFoundException;
 /**
  *
  * @author 
  */
 public class PoliticalMap {
-    public static void main(String[] args) throws FileNotFoundException{
-        Map map = new Map();
+    public static void main(String[] args){
         
-         
+        
     }
 }
